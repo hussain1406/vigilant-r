cb6054a4b97212fbad5e3922e7fa315e90c938f7
Syed Muhammad Hussain <syedhbukhari1492006@gmail.com>
2022-06-12 23:09:20 +0500

    Fixed Some serious bugs.

--- testing.r
+++ testing.r
@@ -1,8 +1,11 @@
 # Code Generated from GitHub
 print("This commit is from GitHub!")
 
+# Code Generated from RStudio
 username = "Hussain"
 print("This is made available from RStudio")
 print(username)
 
 print("Did it Work?")
+
+print("THis was all sent from RStudio.")
