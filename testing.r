78978f43615f178ab2956c0c27a058a0dfe2e8f6
Syed Muhammad Hussain <syedhbukhari1492006@gmail.com>
2022-06-14 15:14:11 +0500

    Ended code

--- testing.r
+++ testing.r
@@ -12,3 +12,12 @@ print("This was all sent from RStudio.")
 # End of Code
 # Have a nice Day
 print("This was sent from VS Code!!!! and it's amazing!")
+m <- c(1:6)
+print(m)
+attributes(m)
+outMatrix <- matrix(data = m, nrow = 3, ncol = 2)
+print(outMatrix)
+attributes(outMatrix)
+dim(outMatrix)
+
+# ---------- End of the code sent From Visual Studio Code ------------- #
